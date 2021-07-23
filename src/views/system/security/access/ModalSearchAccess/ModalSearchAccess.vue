23f143277258b9666db6243ee17eb03c8a07da04
Jhostin Osorio <jhostineosorio@gmail.com>
2021-07-23 18:52:24 -0500

    Correcting line breaks in the access menu

--- src/views/system/security/access/ModalSearchAccess/ModalSearchAccess.vue
+++ src/views/system/security/access/ModalSearchAccess/ModalSearchAccess.vue
@@ -12,7 +12,8 @@
 <script>
 import ModalSearchComponent from '@/components/ModalSearchComponent/ModalSearchComponent.vue'
 import {
-  MODAL_ID, titleNotificationAccess, serverQueryAccess, columnsFilterAccess, clearFiltersAccess,
+  MODAL_ID, titleNotificationAccess, serverQueryAccess, columnsFilterAccess,
+  clearFiltersAccess,
 } from '../ServicesAccess/useVariablesAccess'
 import { loadItemsAccess } from '../ServicesAccess/useServicesAccess'
 
