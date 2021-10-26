e123c319b680231ea2ecf78964142bc1fbf830d4
Jhostin Osorio <jhostineosorio@gmail.com>
2021-10-26 18:03:49 -0500

    Correcting time of load requests in the maintenance of articles

--- src/views/system/products/article/TableArticle/TableArticle.vue
+++ src/views/system/products/article/TableArticle/TableArticle.vue
@@ -20,18 +20,26 @@ import {
   MODAL_ID, stateArticle, columnsArticle, serverQueryArticle, dataTableArticle, titleNotificationArticle, clearStateArticle, combosArticle,
 } from '../ServicesArticle/useVariablesArticle'
 import { getArticleById, loadItemsArticle, sendArticle } from '../ServicesArticle/useServicesArticle'
-import { serverQueryArticleFeatureDetail, clearStateArticleFeatureDetail, clearFiltersArticleFeatureDetail } from '../ServicesArticleFeatureDetail/useVariablesArticleFeatureDetail'
+import {
+  serverQueryArticleFeatureDetail, clearStateArticleFeatureDetail, clearFiltersArticleFeatureDetail, clearDataTableArticleFeatureDetail,
+} from '../ServicesArticleFeatureDetail/useVariablesArticleFeatureDetail'
 import { loadItemsArticleFeatureDetail } from '../ServicesArticleFeatureDetail/useServicesArticleFeatureDetail'
-import { serverQueryArticleRecipeDetail, clearStateArticleRecipeDetail, clearFiltersArticleRecipeDetail } from '../ServicesArticleRecipeDetail/useVariablesArticleRecipeDetail'
+import {
+  serverQueryArticleRecipeDetail, clearStateArticleRecipeDetail, clearFiltersArticleRecipeDetail, clearDataTableArticleRecipeDetail,
+} from '../ServicesArticleRecipeDetail/useVariablesArticleRecipeDetail'
 import { loadItemsArticleRecipeDetail } from '../ServicesArticleRecipeDetail/useServicesArticleRecipeDetail'
-import { clearFiltersArticleTributeDetail, clearStateArticleTributeDetail, serverQueryArticleTributeDetail } from '../ServicesArticleTributeDetail/useVariablesArticleTributeDetail'
+import {
+  clearDataTableArticleTributeDetail, clearFiltersArticleTributeDetail, clearStateArticleTributeDetail, serverQueryArticleTributeDetail,
+} from '../ServicesArticleTributeDetail/useVariablesArticleTributeDetail'
 import { loadItemsArticleTributeDetail } from '../ServicesArticleTributeDetail/useServicesArticleTributeDetail'
 import { clearStateArticleBusinessDetail, serverQueryArticleBusinessDetail } from '../ServicesArticleBusinessDetail/useVariablesArticleBusinessDetail'
 import { loadItemsArticleBusinessDetail } from '../ServicesArticleBusinessDetail/useServicesArticleBusinessDetail'
 import {
+  clearDataTableArticleChildrenDetail,
   clearFiltersArticleChildrenDetail, clearStateArticleChildrenDetail, equalizeStateArticleChildrenDetail, serverQueryArticleChildrenDetail,
 } from '../ServicesArticleChildrenDetail/useVariablesArticleChildrenDetail'
 import { loadItemsArticleChildrenDetail } from '../ServicesArticleChildrenDetail/useServicesArticleChildrenDetail'
+import { clearDataTableArticleChildrenFeatureDetail, clearFiltersArticleChildrenFeatureDetail } from '../ServicesArticleChildrenFeatureDetail/useVariablesArticleChildrenFeatureDetail'
 
 export default {
   name: 'TableArticle',
@@ -43,36 +51,50 @@ export default {
     const openModalFor = async ({ ...row }, actionOpenModal) => {
       dataTableArticle.value.loading = true
       clearStateArticle()
-      if (actionOpenModal === 'show') {
-        stateArticle.value = { ...stateArticle.value, ...row }
-      } else if (actionOpenModal === 'edit') {
-        stateArticle.value = { ...stateArticle.value, ...row }
-        await getArticleById(row._id)
-        await loadCombos(combosArticle, ['unitMeasure'], `${endPointsCombo.grupoUnidad}/${stateArticle.value.idGrupoUnidad}`, 'Unidad de Medida')
-      }
-      if (!stateArticle.value.imagen) stateArticle.value.imagen = ''
       clearStateArticleFeatureDetail()
       clearFiltersArticleFeatureDetail()
+      clearDataTableArticleFeatureDetail()
       clearStateArticleRecipeDetail()
       clearFiltersArticleRecipeDetail()
+      clearDataTableArticleRecipeDetail()
       clearStateArticleTributeDetail()
       clearFiltersArticleTributeDetail()
+      clearDataTableArticleTributeDetail()
       clearStateArticleBusinessDetail()
       clearFiltersArticleChildrenDetail()
+      clearDataTableArticleChildrenDetail()
       clearStateArticleChildrenDetail()
-      equalizeStateArticleChildrenDetail()
+      clearFiltersArticleChildrenFeatureDetail()
+      clearDataTableArticleChildrenFeatureDetail()
       serverQueryArticleFeatureDetail.value.indice = row._id
       serverQueryArticleRecipeDetail.value.indice = row._id
       serverQueryArticleTributeDetail.value.indice = row._id
       serverQueryArticleBusinessDetail.value.indice = row._id
       serverQueryArticleChildrenDetail.value.indice = row._id
-      await Promise.all([
-        loadItemsArticleFeatureDetail(1),
-        loadItemsArticleRecipeDetail(1),
-        loadItemsArticleTributeDetail(1),
-        loadItemsArticleBusinessDetail(1),
-        loadItemsArticleChildrenDetail(1),
-      ])
+
+      if (actionOpenModal === 'show') {
+        stateArticle.value = { ...stateArticle.value, ...row }
+        await Promise.all([
+          loadItemsArticleFeatureDetail(1),
+          loadItemsArticleRecipeDetail(1),
+          loadItemsArticleTributeDetail(1),
+          loadItemsArticleBusinessDetail(1),
+          loadItemsArticleChildrenDetail(1),
+        ])
+      } else if (actionOpenModal === 'edit') {
+        stateArticle.value = { ...stateArticle.value, ...row }
+        await Promise.all([
+          getArticleById(row._id),
+          loadCombos(combosArticle, ['unitMeasure'], `${endPointsCombo.grupoUnidad}/${stateArticle.value.idGrupoUnidad}`, 'Unidad de Medida'),
+          loadItemsArticleFeatureDetail(1),
+          loadItemsArticleRecipeDetail(1),
+          loadItemsArticleTributeDetail(1),
+          loadItemsArticleBusinessDetail(1),
+          loadItemsArticleChildrenDetail(1),
+        ])
+        equalizeStateArticleChildrenDetail()
+      }
+      if (!stateArticle.value.imagen) stateArticle.value.imagen = ''
       dataTableArticle.value.loading = false
       context.root.$bvModal.show(actionOpenModal === 'edit' ? MODAL_ID : `${MODAL_ID}-show`)
     }
